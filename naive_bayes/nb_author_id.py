d65d3b467a1d5ea5f165e1a1fb428af469f5fb80
Nate Snyder <nsnyder@theorchard.com>
2018-02-10 17:28:56 -0500

    Adding lesson 2 project solution

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -12,6 +12,10 @@
     
 import sys
 from time import time
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -25,7 +29,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+cls = GaussianNB()
+t0 = time()
+cls.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = cls.predict(features_test)
+print "predict time:", round(time()-t0, 3), "s"
+
+score = accuracy_score(labels_test, pred)
+print score
+
 
 
 #########################################################
