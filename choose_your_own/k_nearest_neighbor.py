d2fcc1b01c9a691c0933e38f1291400db5b6e24a
Nate Snyder <nsnyder@theorchard.com>
2018-02-19 15:25:11 -0500

    Lesson 5 - k-nearest-neighbors

--- choose_your_own/k_nearest_neighbor.py
+++ choose_your_own/k_nearest_neighbor.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import matplotlib.pyplot as plt
+from sklearn import neighbors
+from sklearn.metrics import accuracy_score
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
 
@@ -16,28 +18,14 @@ grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if l
 bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 
 
-#### initial visualization
-plt.xlim(0.0, 1.0)
-plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
-plt.legend()
-plt.xlabel("bumpiness")
-plt.ylabel("grade")
-plt.show()
-################################################################################
-
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+clf = neighbors.KNeighborsClassifier(weights='distance', algorithm='auto')
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
-
-
-
-
-
-
-
+print 'Accuracy {}'.format(accuracy_score(labels_test, pred))
 try:
     prettyPicture(clf, features_test, labels_test)
 except NameError:
