b04218fc3a8154c48433a4ea8847044c7bbb44f3
Nate Snyder <nsnyder@theorchard.com>
2018-03-03 09:58:48 -0500

    Lession 8.11

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -5,8 +5,9 @@ import numpy
 import matplotlib.pyplot as plt
 import pickle
 
-from outlier_cleaner import outlierCleaner
+from sklearn import linear_model
 
+from outlier_cleaner import outlierCleaner
 
 ### load up some practice data with outliers in it
 ages = pickle.load( open("practice_outliers_ages.pkl", "r") )
@@ -26,7 +27,10 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
-
+reg = linear_model.LinearRegression()
+reg.fit(ages_train, net_worths_train)
+print 'Slope: {}'.format(reg.coef_)
+print 'Score: {}'.format(reg.score(ages_test, net_worths_test))
 
 
 
